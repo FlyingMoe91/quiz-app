377fc36a911c933e6e14b3d3c14fc7ae8153a74c
Patrick Müller <p.moe.mueller@gmail.com>
2022-02-18 18:58:57 +0100

    Refactor answer.js to cards.js in easier code

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 import nav from './js/nav.js';
-import showAnswer from './js/answer.js';
-// import bookmarked from './js/bookmarks.js';
 import allCards from './js/cards.js';
 
 nav();
-showAnswer();
-// bookmarked();
 allCards();
--- js/cards.js
+++ js/cards.js
@@ -3,9 +3,19 @@ function allCards() {
 
   cards.forEach(card => {
     const bookmark = card.querySelector('[data-js="bookmark"]');
+    const answerBox = card.querySelector('[data-js=answer]');
+    const answerButton = card.querySelector('[data-js=showAnswer');
+    const showShow = card.querySelector('[data-js="show"]');
+    const showHide = card.querySelector('[data-js="hide"]');
 
     bookmark.addEventListener('click', () => {
       bookmark.classList.toggle('card__bookmark--selected');
+    });
+
+    answerButton.addEventListener('click', () => {
+      answerBox.classList.toggle('hidden-answer');
+      showShow.classList.toggle('hidden');
+      showHide.classList.toggle('hidden');
     });
   });
 }
